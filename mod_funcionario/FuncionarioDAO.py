b1403630870c53c3c563cd56e35ea7111b445f27
Lenyboy <leonardomartendal@hotmail.com>
2023-09-14 00:25:54 -0300

    Implementação de Token e Key

--- mod_funcionario/FuncionarioDAO.py
+++ mod_funcionario/FuncionarioDAO.py
@@ -5,7 +5,12 @@ from mod_funcionario.FuncionarioModel import FuncionarioDB
 from mod_funcionario.Funcionario import Funcionario
 
 from fastapi import APIRouter
-router = APIRouter()
+
+# import da segurança
+from fastapi import Depends
+import security
+
+router = APIRouter(dependencies=[Depends(security.verify_token), Depends(security.verify_key)])
 
 # Criar as rotas/endpoints: GET, POST, PUT, DELETE
 @router.get("/funcionario/", tags=["Funcionário"])
